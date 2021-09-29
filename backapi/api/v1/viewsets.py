d539b13b7931f2752950c8eaaf6031d0d8084a5d
Usman Arshad <usmangani908.ua@gmail.com>
2021-09-29 17:19:39 +0500

    pagination email

--- backapi/api/v1/viewsets.py
+++ backapi/api/v1/viewsets.py
@@ -3,6 +3,13 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import MeasurementSerializer
 from backapi.models import Measurement
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+
+
+class StandardResultSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page'
+    max_page_size = 100
 
 
 class MeasurementViewSets(ModelViewSet):
--- health_care/settings.py
+++ health_care/settings.py
@@ -166,8 +166,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'medilabclinic.info@gmail.com'
+EMAIL_HOST_PASSWORD = '12345@medilabclinic'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
