046e612a3074104214e85c9930c53c987558f7e4
Usman Arshad <usmangani908.ua@gmail.com>
2021-09-29 17:22:29 +0500

    session

--- health_care/settings.py
+++ health_care/settings.py
@@ -184,7 +184,7 @@ DJANGO_REST_PASSWORDRESET_TOKEN_CONFIG = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
