d25ebdbc490ac12a82607c53946a4d81e12c6841
Usman Arshad <usmangani908.ua@gmail.com>
2021-10-01 13:17:14 +0500

    History API.

--- backapi/api/v1/urls.py
+++ backapi/api/v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .viewsets import MeasurementViewSets
+from .viewsets import MeasurementViewSets, HistoryAPIView
 
 router = DefaultRouter()
 router.register('measurement', MeasurementViewSets , basename='measurement')
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("history/", HistoryAPIView.as_view(), name='history')
 ]
--- backapi/api/v1/viewsets.py
+++ backapi/api/v1/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import filters, permissions
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from .serializers import MeasurementSerializer
 from backapi.models import Measurement
@@ -21,4 +23,28 @@ class MeasurementViewSets(ModelViewSet):
     def get_queryset(self):
         queryset = Measurement.objects.filter(user=self.request.user)
         return queryset
+
+
+class HistoryAPIView(APIView):
+    queryset = Measurement.objects.none
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        response = {}
+        respiratory = self.request.user.user_measurements.filter(category="respiratory").first()
+        oxygen = self.request.user.user_measurements.filter(category="oxygen").first()
+        heart_rate = self.request.user.user_measurements.filter(category="heart_rate").first()
+        viscosity = self.request.user.user_measurements.filter(category="viscosity").first()
+        temperature = self.request.user.user_measurements.filter(category="temperature").first()
+        if respiratory:
+            response.update({"respiratory": respiratory.value})
+        if oxygen:
+            response.update({"oxygen": oxygen.value})
+        if heart_rate:
+            response.update({"heart_rate": heart_rate.value})
+        if viscosity:
+            response.update({"viscosity": viscosity.value})
+        if temperature:
+            response.update({"temperature": temperature.value})
+        return Response(response)
 
