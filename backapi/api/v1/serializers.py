d3e1ed63f2b81344ccff95c93d49e112b67524bb
Usman Arshad <usmangani908.ua@gmail.com>
2021-10-04 18:26:50 +0500

    UPDATE: measurement response.

--- backapi/api/v1/serializers.py
+++ backapi/api/v1/serializers.py
@@ -3,11 +3,19 @@ from backapi.models import Measurement, DeductCreditSetting
 
 
 class MeasurementSerializer(serializers.ModelSerializer):
+    time = serializers.SerializerMethodField(read_only=True)
+    date = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Measurement
-        fields = ['user', 'category', 'value']
-        
+        fields = ['user', 'category', 'value', "time", "date"]
+
+    def get_time(self, obj):
+        return str(obj.created.strftime("%I:%M%p")).lower()
+
+    def get_date(self, obj):
+        return str(obj.created.strftime("%d-%m-%y"))
+
     def validate(self, attrs):
         cleaned_data = super(MeasurementSerializer, self).validate(attrs)
         user = cleaned_data.get('user')
